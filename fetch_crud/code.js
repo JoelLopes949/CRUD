dced4013cbfa161e08076491f4b59c572c300d80
JoelLopes949 <joelyalopes2710@gmail.com>
2026-03-10 20:19:50 -0300

    api

---
+++ fetch_crud/code.js
@@ -0,0 +1,40 @@
+// Defición de variables
+const url = 'http://localhost:3000/api/articulos'
+const contenedor = document.querySelector('tbody')
+let resultados = ''
+
+const modalArticulo = new bootstrap.Modal(document.getElementById('modalArticulo'))
+const formArticulo = document.querySelector('form')
+const descripcion = document.getElementById('descripcion')
+const precio = document.getElementById('precio')
+const stock = document.getElementById('stock')
+let opcion = ''
+
+btnCrear.addEventListener('click', () => {
+    descripcion.value = ''
+    precio.value = ''
+    stock.value = ''    
+    modalArticulo.show()
+    opcion = 'crear'
+})
+
+// función para mostrar los resultados
+const mostrar = (articulos) => {
+    articulos.forEach(articulo => {
+        resultados += `<tr>
+                            <td>${articulo.id}</td>
+                            <td>${articulo.descripcion}</td>
+                            <td>${articulo.precio}</td>
+                            <td>${articulo.stock}</td>
+                            <td class="text-center"><a class="btnEditar btn btn-primary">Editar</a><a class="btnBorrar btn btn-danger">Borrar</a></td>
+                        </tr>`
+    })
+    contenedor.innerHTML = resultados
+}
+
+
+// Procedimiento para mostrar registros
+fetch(url)
+    .then(response => response.json())
+    .then(data => mostrar(data))
+    .catch(error => console.log(error))
